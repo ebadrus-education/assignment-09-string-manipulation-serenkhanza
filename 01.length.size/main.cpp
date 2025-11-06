e92e1b0c8342f8f7f5dcb2b1eb40106a9b55dc63
seren khanzaa azyla <serenkhanzaa@gmail.com>
2025-11-06 10:50:34 +0700

    tambah 01 length size

---
+++ 01.length.size/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string teks = "saya suka ice cream";
+    cout << "Teks: " << teks << endl;
+    cout << "Panjang dengan length(): " << teks.length() << endl;
+    cout << "Panjang dengan size(): " << teks.size() << endl;
+}
